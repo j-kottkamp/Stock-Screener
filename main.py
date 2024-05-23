38153f2698be793ff7e4f224198863707919069b
TIMcre <92308299+TIMcre@users.noreply.github.com>
2024-05-23 11:18:14 +0000

    Error Check

--- main.py
+++ main.py
@@ -15,22 +15,32 @@ def main():
     global stock
     global apikey
     apikey = open("api_key.txt")
+
     while True:
-        if action =="1":
-            stock = input("What Stock do you want to know about?")
-            data = getdata(geturl(f"SYMBOL_SEARCH&keywords={stock}"))
-            basicdata = {
-                "symbol" : data["bestMatches"][0]["1. symbol"],
-                "name"   : data["bestMatches"][0]["2. name"],
-                "region" : data["bestMatches"][0]["4. region"] }
-            price_data = getdata(geturl(f'TIME_SERIES_INTRADAY&{basicdata["symbol"]}=IBM&interval=5min'))
-            price = {
-                "open"   : price_data["Time Series (5min)"][0]["1. open"],
-                "high"   : price_data["Time Series (5min)"][0]["2. high"],
-                "low"    : price_data["Time Series (5min)"][0]["3. low"],
-                "close"  : price_data["Time Series (5min)"][0]["4. close"] }
-            basicdata["price"] = price
-            print(basicdata)
+        try:
+            if action =="1":
+                stock = input("What Stock do you want to know about?")
+                data = getdata(geturl(f"SYMBOL_SEARCH&keywords={stock}"))
+                if len(data) <2:
+                    raise BadRequest
+                basicdata = {
+                    "symbol" : data["bestMatches"][0]["1. symbol"],
+                    "name"   : data["bestMatches"][0]["2. name"],
+                    "region" : data["bestMatches"][0]["4. region"] }
+                price_data = getdata(geturl(f'TIME_SERIES_INTRADAY&{basicdata["symbol"]}=IBM&interval=5min'))
+                if len(price_data) <2:
+                    raise BadRequest
+                price = {
+                    "open"   : price_data["Time Series (5min)"][0]["1. open"],
+                    "high"   : price_data["Time Series (5min)"][0]["2. high"],
+                    "low"    : price_data["Time Series (5min)"][0]["3. low"],
+                    "close"  : price_data["Time Series (5min)"][0]["4. close"] }
+                basicdata["price"] = price
+                print(basicdata)
+        except BadRequest:
+            print("Got no API answer")
+        except:
+            pass
 
 
 
