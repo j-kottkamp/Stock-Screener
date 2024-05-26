f5d53e4c245db135b69455daf4a1d4829bd5f5d8
TIMcre <92308299+TIMcre@users.noreply.github.com>
2024-05-26 10:33:43 +0000

    complete rework of api.py added getting data

--- api.py
+++ api.py
@@ -6,8 +6,9 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 
-# Basics
-def log(text):
+## Basics
+
+def log(text, level="needs to be implemented"): # Implement abilty to have difrent log levels i.a DEBUG PROCESS or whatever get creative also change each call then according to the levels
     if 1 == 0:
         print(text)
     else:
@@ -19,7 +20,8 @@ def generate_random_string(length=8):
 def random_email():
     return f"{generate_random_string()}.{generate_random_string()}@{generate_random_string()}.{generate_random_string()}"
 
-# Web Request Stuff
+## Web Request Stuff
+
 def get_csrf_token(session, url):
     response = session.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -73,67 +75,84 @@ def get_new_api_token(email=random_email()):
 
             return None
 
-# API Key Management
+## API Key Management
 
-# Function to load existing records from file
-def load_api_keys():
+# Functions to load and save API records from file
+def load_from_file():
     try:
         with open('api_keys.json', 'r') as file:
             return json.load(file)
     except: # Not Ideal
+        log("Encounterd Error while loading from file returning empty list")
         return []
 
-# Function to save records to a JSON file
 def save_to_file(api_keys):
-    with open('api_keys.json', 'w') as file:
-        json.dump(api_keys, file)
+    try:
+        with open('api_keys.json', 'w') as file:
+            json.dump(api_keys, file)
+    except: # Not Ideal
+        log("Wasnt abel to Save API Keys to file") # Big Problem
 
-
-# Function to get an API key with amount less than 24 on the current day
-def get_api_key():
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    api_keys = load_api_keys()
-
-    for record in api_keys:
-        if record["amount"] > 0 and record["date"] < current_date:
-            record["amount"] = 0
-            record["date"] = current_date
-            save_to_file(api_keys)
-            return record["api_key"]
-
-        if record["amount"] < 24 and record["date"] == current_date:
-            record["amount"] += record["amount"]
-            return record["api_key"]
-
-    
-    record = save_api_key(api_keys, get_new_api_token())
-    return record["api_key"]
-
-
-
-
-# Example usage:
-# Function to save API key record
-def save_api_key(api_keys, api_key, amount=0):
+# Function for Consistent data Structure / Creating new entries
+def convert_to_record(api_key, amount=0):
     record = {
         "api_key": api_key,
         "amount": amount,
         "date": datetime.datetime.now().strftime("%Y-%m-%d")
     }
-    api_keys.append(record)
-    save_to_file(api_keys)
+    log(f"convertet apiKey:|{api_key}| to Record:|{record}|")    
     return record
 
+# Function to get an API key with amount less than 24 on the current day and update data lazyly
+# This is a fucntion that can be called by the user if only_key is False the whole record will be returned
+def get_api_key(only_key=True):
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    api_keys = load_from_file()
+    log(f"Loaded Api Keys: {api_keys}")
 
-# Retrieving an API key with amount less than 24 on the current day
-api_key = get_api_key()
-if api_key:
-    print(f"Got API key: {api_key}")
-else:
-    print("No available API keys with amount less than 24 on the current day.")
+    for record in api_keys:
+        if record["amount"] > 0 and record["date"] < current_date:
+            record["amount"] = 0
+            record["date"] = current_date
+
+            save_to_file(api_keys)
+            return record["api_key"]
+
+        elif record["amount"] < 24 and record["date"] == current_date:
+            record["amount"] = record["amount"] + 1
+
+            save_to_file(api_keys)
+            return record["api_key"]
 
 
+    log("Was not abel to find valid key getting new one...")
+    trys = 10 # change this if we have used up too many email adresses
 
+    for i in range(trys):
+        api_key = get_new_api_token()
+        if api_key:
+            record = convert_to_record(api_key)
+            api_keys.append(record)
+            log(f"Returning Valid Api Key {record}")
+            if only_key:
+                return record["api_key"]
+            return record
+            
+        else:
+            log(f"API Key was None trying again for the {i}th time..")
 
+    log(f"Wasnt abel to get API Key after {trys} raising Error | maybe try raising 'trys'")
+    raise Exception
 
-#print(get_new_api_token(random_email))
+## API Requests
+
+def geturl(unique):
+    url = "https://www.alphavantage.co/query?function={unique}&apikey={get_api_key}"
+    log(f"Prepared url: {url}")
+    return url
+
+def get(unique):
+    url = geturl(unique)
+    data = requests.get(url).json()   
+    log(f"Got data back from API: {data}")
+    return data
