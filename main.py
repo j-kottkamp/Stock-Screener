8acca2443b04c5fd007a12104a533d9c545a1d5a
TIMcre <92308299+TIMcre@users.noreply.github.com>
2024-05-26 10:33:05 +0000

    implementet new api request

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 import requests
-
-from api import api_key()
-
-def geturl(unique):
-    return f'https://www.alphavantage.co/query?function={unique}&apikey={api_key()}'
+import api
 
 
-
-def getdata(url):
-    return requests.get(url).json()
     
 topics = ["blockchain", "earnings", "ipo", "mergers_and_acquisitions", "financial_markets", "economy_fiscal", "economy_monetary", "economy_macro", "energy_transportation", "finance", "life_sciences", "manufacturing", "real_estate", "retail_wholesale", "technology"]
 
 
 def main():
     global stock
-    global apikey
-    #apikey = open("api_key.txt")
 
     while True:
         try:
             if action =="1":
                 stock = input("What Stock do you want to know about?")
-                data = getdata(geturl(f"SYMBOL_SEARCH&keywords={stock}"))
+                data = api.get(f"SYMBOL_SEARCH&keywords={stock}")
                 if len(data) <2:
-                    raise BadRequest
+                    raise ValueError
                 basicdata = {
                     "symbol" : data["bestMatches"][0]["1. symbol"],
                     "name"   : data["bestMatches"][0]["2. name"],
                     "region" : data["bestMatches"][0]["4. region"] }
-                price_data = getdata(geturl(f'TIME_SERIES_INTRADAY&{basicdata["symbol"]}=IBM&interval=5min'))
+                price_data = api.get(f'TIME_SERIES_INTRADAY&{basicdata["symbol"]}=IBM&interval=5min')
                 if len(price_data) <2:
-                    raise BadRequest
+                    raise ValueError
                 price = {
                     "open"   : price_data["Time Series (5min)"][0]["1. open"],
                     "high"   : price_data["Time Series (5min)"][0]["2. high"],
@@ -44,15 +35,15 @@ def main():
                 i = 0
                 search = input("Search by Topic or by Stock?\n\'1\' for Topic\n\'2\' for Stock\n\'.\' for all News")
                 if search == ".":
-                    news = getdata(geturl("NEWS_SENTIMENT"))
+                    news = api.get("NEWS_SENTIMENT")
                 elif search == "2":
                     ticker = input("Enter Ticker Symbol")
-                    news = getdata(geturl(f"NEWS_SENTIMENT&tickers={ticker}"))
+                    news = api.get(f"NEWS_SENTIMENT&tickers={ticker}")
                 elif search == "1":
                     while True:
                         topic = input("Enter Topic")
                         if topic in topics:
-                            news = getdata(geturl(f"NEWS_SENTIMENT&topics={topic}"))
+                            news = api.get(f"NEWS_SENTIMENT&topics={topic}")
                             break
                         else:
                             print("Cant find Topic. Please choose one of the provided topics", topics)
@@ -65,7 +56,7 @@ def main():
                 for i in newsfeed:
                     print(f"News!\n>>>{newsfeed['title']}<<<\n{newsfeed['summary']}\nSource:{newsfeed['newsurl']}\nExperts say, the Market is {newsfeed['flow']}.")
                     i+=1
-        except BadRequest:
+        except ValueError:
             print("Got no API answer")
         except:
             pass
